60207a9de47712781d057a16198457eeee1a5d20
Serena <153142569+LIXINYI33@users.noreply.github.com>
2024-10-14 11:34:19 +0000

    traits

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -8,12 +8,16 @@
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 trait AppendBar {
-    fn append_bar(self) -> Self;
+    fn append_bar(&mut self) -> Self;
 }
 
+impl AppendBar for Vec<String>{
+    fn append_bar(&mut self)->Self{
+        self.push("Bar".to_string());
+        self.to_vec()
+    }
+}
 // TODO: Implement trait `AppendBar` for a vector of strings.
 
 #[cfg(test)]
